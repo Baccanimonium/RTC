c42cf704113580078fcc8e80dbec4f8a6e2ee590
Onichaan <yaroslav.prokopchuk@publicismedia.com>
2021-06-16 14:37:52 +0300

    Заполнены инит миграции

--- schema/000001_init.down.sql
+++ schema/000001_init.down.sql
@@ -1,1 +1,8 @@
 DROP TABLE users;
+DROP TABLE doctor;
+DROP TABLE patient;
+DROP TABLE course;
+DROP TABLE schedule;
+DROP TABLE consultation;
+DROP TABLE event;
+DROP TABLE schedule_event;
--- schema/000001_init.up.sql
+++ schema/000001_init.up.sql
@@ -1,24 +1,70 @@
 CREATE TABLE users
 (
-    id            serial       not null unique,
-    name          varchar(255) not null,
-    login         varchar(255) not null unique,
-    password      varchar(255) not null,
-    about         varchar(255),
-    address       varchar(255),
-    phone         varchar(255),
+    id                 serial       not null unique,
+    name               varchar(255) not null,
+    login              varchar(255) not null unique,
+    password           varchar(255) not null,
+    about              varchar(255),
+    address            varchar(255),
+    phone              varchar(255),
 );
 
-CREATE TABLE doctors (
+CREATE TABLE doctor (
+    id                 serial not null unique,
+    id_user            int    references users (id) not null,
+    salary             double,
+    qualifications     varchar(255),
+    contacts           varchar(255)
+)
 
+CREATE TABLE patient (
+    id                 serial not null unique,
+    id_user            int    references users (id) not null,
+    description        varchar(500),
+    recovered          boolean      not null default false
+)
+
+CREATE TABLE course (
+    id                 serial not null unique,
+    id_patient         int    references patient (id) not null,
+    id_users           int    references users (id) not null,
+    title              varchar(100),
+    description        varchar(500),
+    time_start         varchar(10),
+    time_end           varchar(10),
 );
 
-
-CREATE TABLE patients (
-
+CREATE TABLE schedule (
+    id                 serial not null unique,
+    id_user            int    references users (id) not null,
+    title              varchar(100),
+    description        varchar(500),
 );
 
-CREATE TABLE group (
-
+CREATE TABLE consultation (
+    id                 serial not null unique,
+    id_patient         int    references patient (id) not null,
+    id_users           int    references users (id) not null,
+    id_course          int    references course (id),
+    title              varchar(100),
+    time_start         varchar(16),
+    time_end           varchar(16),
 );
 
+CREATE TABLE event (
+    id                 serial not null unique,
+    time_start         varchar(16),
+    time_end           varchar(16),
+    title              varchar(100),
+    id_course          int    references course (id),
+    type               varchar(100),
+    description        varchar(500),
+    accepted           boolean      not null default false,
+)
+
+CREATE TABLE schedule_event
+(
+    id                 serial         not null unique,
+    id_schedule        int references schedule (id) on delete cascade not null,
+    id_course          int references course (id) on delete cascade not null
+);
